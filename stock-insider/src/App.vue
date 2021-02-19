32528f32c50bb5b810031738d0d89657e8bd890b
Minwoo Jeon <jeonminwoo0625@gmail.com>
2021-02-19 11:33:48 +0900

    Add files via upload

---
+++ stock-insider/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <v-app-bar app>
+      <Header />
+    </v-app-bar>
+    <v-main>
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
+    </v-main>
+    <v-footer app>
+      <Footer />
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
+
+export default {
+  components: { Header, Footer },
+};
+</script>
---
+++ stock-insider/src/store.js
@@ -0,0 +1,43 @@
+import Vue from "vue";
+import Vuex from "vuex";
+
+Vue.use(Vuex);
+
+export const store = new Vuex.Store({
+  state: {
+    stockTradeInfos: [],
+    statisticsInfos: [],
+    selectedTraderInfos: [],
+  },
+
+  getters: {
+    getStatisticsInfosCnt: (state) => {
+      return state.statisticsInfos.length;
+    },
+    getSelectedTraderInfosCnt: (state) => {
+      return state.selectedTraderInfos.length;
+    },
+  },
+
+  mutations: {
+    setStockTradeInfos: (state, payload) => {
+      state.stockTradeInfos = payload;
+    },
+
+    setStatisticsInfos: (state, payload) => {
+      state.statisticsInfos = payload;
+    },
+
+    setSelectedTraderInfos: (state, payload) => {
+      state.selectedTraderInfos = payload;
+    },
+  },
+
+  // 비동기 실행의 경우 / 비즈니스 로직 처리(commit 이전의 중복 검사 등)
+  actions: {
+    setStatisticsInfos: ({ commit }, payload) => {
+      // 여기에 비즈니스 로직들 처리 후
+      commit("setStatisticsInfos", payload);
+    },
+  },
+});
